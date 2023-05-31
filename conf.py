6f020db7f7814549994ea59f749fd7517fb848f8
dwelke <dominik.welke@ae.mpg.de>
2023-05-31 17:28:18 +0200

    Nikola auto commit.
    Nikola version: 8.2.4

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "DW"  # (translatable)
 BLOG_TITLE = "Dominik Welke"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://dominikwelke.github.io"
+SITE_URL = "http://dominikwelke.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
